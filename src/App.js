be79d7c754df3eb7dc146d4261b0951eebaebd69
Botar Ando <botar.ando@guidesmiths.com>
2022-04-30 03:21:56 +0200

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+import Floor from './components/Floor';
+import { useLocalStorage } from './hooks/useStorage/useStorage';
+
+import classes from './App.module.scss';
 
 function App() {
+  const [projectList, setProjectList] = useLocalStorage('projectList', [
+    'impostors-genesis-aliens',
+    'veefriends',
+    'veefriends-series-2',
+    'thehabibiz',
+    'thebibiz',
+    'curiocardswrapper',
+    'satoshirunnersofficial',
+    'genetic-chain-midnight-runner-pass',
+    'superfarm',
+    'samot-club',
+    'we-are-kloud',
+    '0xidentitiesgen1',
+  ]);
+
+  const [projectLink, setProjectLink] = useState();
+
+  const handleChange = e => {
+    setProjectLink(e.target.value);
+  };
+
+  const handleSave = () => {
+    const lastSlashIndex = projectLink.lastIndexOf('/');
+    const name = projectLink.substring(lastSlashIndex + 1);
+    setProjectList([...projectList, name]);
+    setProjectLink('')
+  };
+
+  const handleRemove = project => {
+    setProjectList(projectList.filter(p => p !== project))
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={classes.pageHeader}>FLOOR CHECK</div>
+      <div className={classes.form}>
+        <label htmlFor="projectLink">Add OpenSea project link</label>
+        <input
+          id="projectLink"
+          type="text"
+          value={projectLink}
+          onChange={handleChange}
+        />
+        <button onClick={handleSave}>Save</button>
+      </div>
+      <div className={classes.floors}>
+        {projectList.map(project => (
+          <Floor key={project} project={project} handleRemove={handleRemove} />
+        ))}
+      </div>
     </div>
   );
 }
