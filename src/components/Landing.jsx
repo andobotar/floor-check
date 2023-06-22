ef141ba76b373a921c6d5913832d551b2196bbe5
Botar Ando <botar.ando@guidesmiths.com>
2023-06-23 00:35:00 +0200

    Add out of service message to landing page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -5,6 +5,13 @@ import classes from './Landing.module.scss';
 export default function TestPage({ setPage }) {
   return (
     <div className={classes.container}>
+      <div className={classes.outOfServiceMessage}>
+        <h4>Opensea fucked us over by changing its endpoints</h4>
+        <h5>
+          I'm working on a solution but I don't have a lot of time so it's gonna
+          take a while. Sorry.
+        </h5>
+      </div>
       <div onClick={() => setPage('floorChecker')} className={classes.button}>
         FLOOR CHECKER - ANY NFT
       </div>
